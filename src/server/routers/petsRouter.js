7801b678c86fe544b35755592dc237f5bb706528
AlbSag <92945087+TryoSag@users.noreply.github.com>
2022-06-10 11:10:07 +0200

    added editPet on pets router

--- src/server/routers/petsRouter.js
+++ src/server/routers/petsRouter.js
@@ -3,6 +3,7 @@ const {
   getPets,
   deletePet,
   createPet,
+  editPet,
 } = require("../controllers/petsControllers");
 
 const petsRouter = express.Router();
@@ -10,5 +11,6 @@ const petsRouter = express.Router();
 petsRouter.get("/", getPets);
 petsRouter.delete("/:id", deletePet);
 petsRouter.post("/create", createPet);
+petsRouter.put("/edit", editPet);
 
 module.exports = petsRouter;
